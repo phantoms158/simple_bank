a4c1227d62ad895616fd1affff925408ee85ab1c
Phan Trong <trongdt.working@gmail.com>
2022-11-28 21:55:58 +0700

    implement grpc API to create and login user and how to use evans

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net"
 
 	_ "github.com/lib/pq"
 	"github.com/phantoms158/simple_bank/api"
 	db "github.com/phantoms158/simple_bank/db/sqlc"
+	"github.com/phantoms158/simple_bank/gapi"
+	"github.com/phantoms158/simple_bank/pb"
 	"github.com/phantoms158/simple_bank/util"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -20,14 +25,39 @@ func main() {
 		log.Fatal("cannot connect to db: ", err)
 	}
 	store := db.NewStore(conn)
+	runGrpcerver(config, store)
+}
+
+func runGrpcerver(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("Cannot create server: ", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterSimpleBankServer(grpcServer, server)
+	reflection.Register(grpcServer)
+
+	listener, err := net.Listen("tcp", config.GRPCServerAddress)
+	if err != nil {
+		log.Fatal("cannot create listener")
+	}
+
+	log.Printf("start gRPC server at %s", listener.Addr().String())
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("cannot start gRPC server")
+	}
+}
+
+func runGinServer(config util.Config, store db.Store) {
 	server, err := api.NewServer(config, store)
 	if err != nil {
 		log.Fatal("Cannot create server: ", err)
 	}
 
-	err = server.Start(config.ServerAddress)
+	err = server.Start(config.HTTPServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
-
 }
